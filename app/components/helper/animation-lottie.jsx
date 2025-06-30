689b1658306d0324d2635c05fa4a688728154a6a
Dishu Bansal <dishu.bansal@ryerson.ca>
2025-06-30 00:07:10 -0400

    Fixed minor error

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,6 +1,7 @@
 "use client"
-
-import Lottie from "lottie-react";
+import dynamic from 'next/dynamic';
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+// import Lottie from "lottie-react";
 
 const AnimationLottie = ({ animationPath, width }) => {
   const defaultOptions = {
